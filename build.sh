336cd9b5ad41db42216bb6f7dbb75edb8efcf963
BonisOleg <olegbonislav@gmail.com>
2025-06-25 12:18:18 +0300

    Fix Render deployment: move migrations to startCommand and update config

--- build.sh
+++ build.sh
@@ -9,7 +9,4 @@ mkdir -p logs
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
-
-# Apply database migrations
-python manage.py migrate 
+python manage.py collectstatic --no-input 
